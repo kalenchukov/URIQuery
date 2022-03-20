ecf783e85ac114b3a6c5828005b3a977055afdf8
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 11:37:00 +0300

    Обновлён копирайт

--- src/main/java/dev/kalenchukov/uriquery/package-info.java
+++ src/main/java/dev/kalenchukov/uriquery/package-info.java
@@ -4,12 +4,4 @@
  * E-mail: mailto:aleksey.kalenchukov@yandex.ru
  */
 
-/**
- * Сборщик / разборщик параметров URI
- *
- * @author <a href="mailto:aleksey.kalenchukov@yandex.ru">Алексей Каленчуков</a>
- * @copyright © 2021 Алексей Каленчуков
- * @package URIQuery
- * @version 1.1.0
- */
 package dev.kalenchukov.uriquery;
